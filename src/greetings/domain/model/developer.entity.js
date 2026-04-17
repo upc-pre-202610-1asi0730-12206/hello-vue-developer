7496bf93b671733e1020410a9e573942f6d49122
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-17 18:18:23 -0500

    feat(developer): add developer entity.

---
+++ src/greetings/domain/model/developer.entity.js
@@ -0,0 +1,20 @@
+export class Developer {
+    #firstName;
+    #lastName;
+    constructor(firstName, lastName) {
+        this.#firstName = firstName?.trim() || '';
+        this.#lastName = lastName?.trim() || '';
+    }
+    get firstName() {
+        return this.#firstName;
+    }
+    get lastName() {
+        return this.#lastName;
+    }
+
+    get fullName() {
+        return this.#firstName && this.#lastName
+            ? `${this.#firstName} ${this.#lastName}`
+            : "Unknown";
+    }
+}
